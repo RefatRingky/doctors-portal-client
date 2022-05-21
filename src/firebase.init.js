307a61fbe88e4a26cf90e18aa8b6b0283e53954d
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-05-21 10:43:11 +0600

    firebase connect

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_kDV-Atu1fL5iFa7F3u3psE9lqDSMOpk",
+  authDomain: "doctors-portal-5332b.firebaseapp.com",
+  projectId: "doctors-portal-5332b",
+  storageBucket: "doctors-portal-5332b.appspot.com",
+  messagingSenderId: "60047024855",
+  appId: "1:60047024855:web:c6700b6e0683e0cd158692"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
